a84639647efa2d685dd76fbb2649a68e36b07bfb
shinbom <tlsqha@gmail.com>
2024-05-03 10:45:06 +0900

    feat : tagList vanilla extract 추가

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,7 @@
 
 export { default as TestComponent } from './components/TestComponent'
 export { Icon } from './components/Icon'
+export { TagList } from './components/TagList'
 export { Tag } from './components/Tag'
 export { Warning } from './components/Warning'
 export { Avatar } from './components/Avatar'
