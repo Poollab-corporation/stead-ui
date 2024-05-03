3c206055e1f720e5702e1384883c71b9984f6d7a
shinbom <tlsqha@gmail.com>
2024-05-03 10:28:34 +0900

    feat : badge vanilla extract 적용

---
+++ src/lib/components/Badge/Badge.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Badge } from '.'
+
+const meta = {
+  title: 'Component/Badge',
+  component: Badge,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    text: {
+      type: 'string',
+      defaultValue: '직접 추가',
+      description: '지원 경로 및 불합격 표기',
+    },
+    type: {
+      options: ['standard', 'primary', 'warning', 'gray', 'proceeding', 'success'],
+      control: { type: 'select' },
+      description: '타입 선택',
+    },
+    isDisabled: {
+      type: 'boolean',
+      defaultValue: false,
+      description: '비활성화',
+    },
+
+    rounded: {
+      type: 'number',
+      defaultValue: 4,
+      description: '라운드',
+    },
+  },
+} satisfies Meta<typeof Badge>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    text: '직접 추가',
+    type: 'standard',
+    isDisabled: false,
+    rounded: 4,
+  },
+}
---
+++ src/lib/components/Badge/index.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { badge } from '@/lib/components/Badge/styles.css.ts'
+
+interface BadgeProps {
+  readonly text: string
+  readonly type?: 'standard' | 'primary' | 'warning' | 'gray' | 'proceeding' | 'success'
+  readonly isDisabled?: boolean
+  readonly rounded?: number
+}
+
+export const Badge = ({ text, type = 'standard', isDisabled, rounded = 4 }: BadgeProps) => {
+  return (
+    <div
+      className={badge({
+        type,
+        isDisabled,
+      })}
+      style={{ borderRadius: `${rounded}px` }}
+    >
+      {text}
+    </div>
+  )
+}
