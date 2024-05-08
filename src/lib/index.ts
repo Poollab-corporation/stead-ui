33eed6d8e1e95a637b11f7e934998b9fd04137b8
kyungfuture <miraecod@gmail.com>
2024-05-08 19:39:36 +0900

    feat: AuthorityBadge 컴포넌트 제작

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
 export { ArrowToggle } from './components/ArrowToggle'
+export { AuthorityBadge } from './components/AuthorityBadge'
 export { Avatar } from './components/Avatar'
 export { Badge } from './components/Badge'
 export { Button } from './components/Button'
