d9f2cb6b6ecaf03d3e1bea1b9b5980cde1697edb
kyungfuture <miraecod@gmail.com>
2024-05-24 16:38:39 +0900

    chore: recoil 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './normalize.css'
+import { RecoilRoot } from 'recoil'
 
 // @ts-ignore
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
 )
