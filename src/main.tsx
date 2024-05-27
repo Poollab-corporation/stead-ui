5813d845dffea05e34f893cc1352cd964510e204
kyungfuture <miraecod@gmail.com>
2024-05-27 14:58:44 +0900

    remove: recoil

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './normalize.css'
-import { RecoilRoot } from 'recoil'
 
 // @ts-ignore
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <App />
   </React.StrictMode>,
 )
