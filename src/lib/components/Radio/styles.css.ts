41036e553e4478b4429dede9ab85f6a42680e966
kyungfuture <miraecod@gmail.com>
2024-05-29 17:13:22 +0900

    feat: Radio 컴포넌트

--- src/lib/components/Radio/styles.css.ts
+++ src/lib/components/Radio/styles.css.ts
@@ -1,5 +1,5 @@
-import { style } from '@vanilla-extract/css'
 import { theme } from '@/globalTheme.css.ts'
+import { globalStyle, style } from '@vanilla-extract/css'
 import { recipe } from '@vanilla-extract/recipes'
 
 export const wrapper = style({
@@ -7,196 +7,162 @@ export const wrapper = style({
   position: 'relative',
   alignItems: 'center',
   gap: '4px',
-});
+})
 
 export const inputWrapper = style({
   position: 'relative',
-});
+})
 
 export const input = style({
   position: 'absolute',
   width: '100%',
   height: '100%',
   visibility: 'hidden',
-});
+})
 
 export const label = style({
   display: 'flex',
   alignItems: 'center',
   gap: '4px',
+  cursor: 'pointer',
+})
+
+export const text = style({
+  marginBottom: '-2px',
   ...theme.fonts[14],
   fontWeight: theme.fontWeights.medium,
-  cursor: 'pointer',
-});
+  color: theme.colors.neutralGray800,
+})
+
+export const mark = style({
+  marginBottom: '-8px',
+  ...theme.fonts[14],
+  fontWeight: theme.fontWeights.medium,
+  color: theme.colors.red500,
+})
 
 export const radio = recipe({
-  base :{
-    selectors : {
-      ['&:before'] : {
+  base: {
+    selectors: {
+      ['&:before']: {
         content: '""',
         display: 'block',
-        position:'relative',
+        position: 'relative',
         cursor: 'pointer',
         borderRadius: '16px',
+        backgroundColor: theme.colors.neutralGray200,
       },
-      ['&:after'] : {
-        content : '',
-        borderRadius : '8px',
-        position:'absolute',
+      ['&:after']: {
+        content: '',
+        borderRadius: '8px',
+        position: 'absolute',
         top: '50%',
-        transform : 'translateY(-50%)'
-      }
-    }
-  },
-  variants : {
-    state : {
-      positive : {
-        selectors: {
-          ['&:after'] : {
-            background : theme.colors.blue500
-          }
-        }
+        transform: 'translateY(-50%)',
+        backgroundColor: theme.colors.white,
       },
-      negative : {
-        selectors: {
-          ['&:after'] : {
-            background : theme.colors.red500
-          }
-        }
-      },
-      default : {
-        selectors: {
-          ['&:after'] : {
-            background : theme.colors.neutralGray800
-          }
-        }
-      }
     },
-    size : {
-      sm : {
-        selectors : {
-          ['&:before'] :{
-            width:'16px',
-            height:'16px',
+  },
+  variants: {
+    state: {
+      primary: {
+        selectors: {
+          ['&:before']: {
+            border: `1px solid ${theme.colors.blue400}`,
           },
-          ['&:after'] : {
+          ['&:after']: {
+            display: 'none',
+          },
+        },
+      },
+      secondary: {
+        selectors: {
+          ['&:before']: {
+            border: `1px solid ${theme.colors.neutralGray700}`,
+          },
+          ['&:after']: {
+            display: 'none',
+          },
+        },
+      },
+    },
+    size: {
+      md: {
+        selectors: {
+          ['&:before']: {
+            width: '14px',
+            height: '14px',
+          },
+          ['&:after']: {
             width: '8px',
             height: '8px',
-            left: '4px'
-          }
-        }
-      },
-      md : {
-        selectors : {
-          ['&:before'] :{
-            width:'22px',
-            height:'22px',
+            left: '4px',
           },
-          ['&:after'] : {
-            width: '12px',
-            height: '12px',
-            left: '5px'
-          }
-        }
-      }
+        },
+      },
+      lg: {
+        selectors: {
+          ['&:before']: {
+            width: '18px',
+            height: '18px',
+          },
+          ['&:after']: {
+            width: '10px',
+            height: '10px',
+            left: '5px',
+          },
+        },
+      },
     },
-    checked : {
-      true : {
+    checked: {
+      true: {
         position: 'relative',
-        selectors : {
-          ['&:after'] : {
+        selectors: {
+          ['&:after']: {
             display: 'block',
-          }
-        }
+          },
+        },
       },
-      false : {
+      false: {
         position: 'relative',
-        selectors : {
-          ['&:after'] : {
+        selectors: {
+          ['&:after']: {
             display: 'none',
-          }
-        }
-      }
-    }
+          },
+        },
+      },
+    },
+    disabled: {
+      true: {
+        opacity: 0.5,
+      },
+    },
   },
-  compoundVariants : [
+  compoundVariants: [
     {
-      variants : {
-        state : 'default',
-        checked : true
+      variants: {
+        state: 'primary',
+        checked: true,
       },
-      style : {
-        selectors : {
-          ['&:before'] : {
-            background : theme.colors.neutralGray200
-          }
-        }
-      }
+      style: {
+        selectors: {
+          ['&:before']: {
+            backgroundColor: theme.colors.blue400,
+          },
+        },
+      },
     },
     {
-      variants : {
-        state : 'default',
-        checked : false
+      variants: {
+        state: 'secondary',
+        checked: true,
       },
-      style : {
-        selectors : {
-          ['&:before'] : {
-            background : theme.colors.neutralGray200
-          }
-        }
-      }
+      style: {
+        selectors: {
+          ['&:before']: {
+            backgroundColor: theme.colors.neutralGray700,
+          },
+        },
+      },
     },
-    {
-      variants : {
-        state : 'positive',
-        checked : true
-      },
-      style : {
-        selectors : {
-          ['&:before'] : {
-            background : theme.colors.blue200
-          }
-        }
-      }
-    },
-    {
-      variants : {
-        state : 'positive',
-        checked : false
-      },
-      style : {
-        selectors : {
-          ['&:before'] : {
-            background : theme.colors.blue100
-          }
-        }
-      }
-    },
-    {
-      variants : {
-        state : 'negative',
-        checked : true
-      },
-      style : {
-        selectors : {
-          ['&:before'] : {
-            background : theme.colors.red200
-          }
-        }
-      }
-    },
-    {
-      variants : {
-        state : 'negative',
-        checked : false
-      },
-      style : {
-        selectors : {
-          ['&:before'] : {
-            background : theme.colors.red100
-          }
-        }
-      }
-    }
-  ]
+  ],
 })
