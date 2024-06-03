10ddc589892eede25cc6b904b6f82079d217a524
kyungfuture <miraecod@gmail.com>
2024-06-03 15:30:24 +0900

    feat: AvatarMultiple 컴포넌트

--- src/lib/components/Avatar/index.tsx
+++ src/lib/components/Avatar/index.tsx
@@ -1,5 +1,4 @@
-import { FONT_SIZE_OPTIONS, SIZE_OPTIONS } from './Avatar.constants'
-import { AvatarProps } from './Avatar.interface'
+import { theme } from '@/globalTheme.css'
 import {
   avatar,
   avatarBackground,
@@ -10,14 +9,15 @@ import {
   avatarSize,
 } from '@/lib/components/Avatar/styles.css.ts'
 import { assignInlineVars } from '@vanilla-extract/dynamic'
+import { FONT_SIZE_OPTIONS, SIZE_OPTIONS } from './Avatar.constants'
+import { AvatarProps } from './Avatar.interface'
 
 export const Avatar = ({ size = 'md', alt = 'avatar', src, isBorder, disabled }: AvatarProps) => {
   const isImage = !src?.includes('rgb')
-
   const firstName = alt?.charAt(0)
   const sizeToUse = `${String(SIZE_OPTIONS[size])}px`
   const fontSize = `${String(FONT_SIZE_OPTIONS[size])}px`
-  const border = isBorder ? '1px solid red' : 'none'
+  const border = isBorder ? `1px solid ${theme.colors.white}` : 'none'
   const disabledStyle = disabled ? 'block' : 'none'
 
   if (!isImage) {
@@ -36,6 +36,7 @@ export const Avatar = ({ size = 'md', alt = 'avatar', src, isBorder, disabled }:
       </div>
     )
   }
+
   return (
     <div
       className={avatar}
