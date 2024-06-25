b29974f5752f83a9c228ab8c912d43631866c31c
39wss <kimhanseolzz@gmail.com>
2024-06-25 10:00:54 +0900

    fix: media 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './globalTheme.css'
+import { AvatarMultiple, Button, Tooltip } from './lib'
 
 function App() {
   return (
     <div>
       <h1>STEAD UI</h1>
+      <AvatarMultiple members={[]} />
+      <Tooltip text="?">
+        <Button text={'Click me'} />
+      </Tooltip>
     </div>
   )
 }
